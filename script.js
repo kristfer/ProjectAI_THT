b7af984d16f34e284e013d903116b92c2fe0fa51
williamtanuwijaya <will.tw0807@gmail.com>
2024-05-12 18:16:51 +0700

    update

--- script.js
+++ script.js
@@ -1,62 +1,75 @@
 function Diagnosa() {
-    let symtomps = document.querySelectorAll('input[name="symtomp"]:checked');
+  let selectedSymptoms = document.querySelectorAll('input[name="symtomp"]:checked');
+  let totalSelectedSymptoms = selectedSymptoms.length;
 
-let symtompCode = [];
-symtomps.forEach((symtomp) => {
-    symtompCode.push(symtomp.value);
-});
+  let presentasePenyakit = {};
 
-let namaPenyakit = [
-    {name:'CONTRACT ULCERS', symtomps:[3, 16]},
-    {name:'ABAES PARAFARINGEAL', symtomps:[3, 19]},
-    {name:'ABAES PERITONAILER', symtomps:[1,2,7,14,16,22]},
-    {name:'BAROTITIS MEDIA', symtomps:[2,6]},
-    {name:'DEVIASI SEPTUM', symtomps:[1,5,6,15,25,29]},
-    {name:'FARINGITIS', symtomps:[1,3,7,13,14]},
-    {name:'KANKER LARING', symtomps:[3,4,7,13,16,23,24]},
-    {name:'KANKER LEHER & KEPALA', symtomps:[3,12,15,21,30,31]},
-    {name:'KANKER LEHER METASTATIK', symtomps:[12]},
-    {name:'KANKER NASOFARING', symtomps:[5,15]},
-    {name:'KANKER TONSIL', symtomps:[7, 12]},
-    {name:'LARINGITIS', symtomps:[1,3,14,19,37]},
-    {name:'NEURONITIS VESTIBULARIS', symtomps:[10,17]},
-    {name:'OSTEOSKLEROSIS', symtomps:[20,35]},
-    {name:'OTITIS MEDIA AKUT', symtomps:[1,6,10,32]},
-    {name:'MENIERE', symtomps:[6,10,34,36]},
-    {name:'TONSILITIS', symtomps:[1,2,3,4,7,10]},
-    {name:'TUMOR SYARAF PENDENGARAN', symtomps:[2,20,38]},
-    {name:'VERTIGO POSTULAR', symtomps:[17]},
-    {name:'SINUSITIS MAKSILARIS', symtomps:[1,2,4,5,8,9,11,28,33]},
-    {name:'SINUSITIS FRONTALIS', symtomps:[1,2,4,5,8,9,11,18]},
-    {name:'SINUSITIS ETMOIDALIS', symtomps:[1,2,4,5,8,9,11,18,26,27]},
-    {name:'SINUSITIS SFENOIDALIS', symtomps:[1,2,4,5,6,8,9,11,12]},
-    {name:'PERUT', symtomps:[1,2,3,4]},
-];
+  selectedSymptoms.forEach((selectedSymptom) => {
+    let selectedSymptomValue = parseInt(selectedSymptom.value);
 
-let Pencocokan = {};
-namaPenyakit.forEach((penyakit) => {
-    penyakit.symtomps.forEach((symtomp) => {
-        if(symtompCode.indexOf(symtomp.toString()) !== -1) {
-            if(!Pencocokan[penyakit.name]) {
-                Pencocokan[penyakit.name] = true;
-            }
+    let namaPenyakit = [
+      { name: 'CONTRACT ULCERS', symtomps: [3, 16] },
+      { name: 'ABAES PARAFARINGEAL', symtomps: [3, 19] },
+      { name: 'ABAES PERITONAILER', symtomps: [1, 2, 7, 14, 16, 22] },
+      { name: 'BAROTITIS MEDIA', symtomps: [2, 6] },
+      { name: 'DEVIASI SEPTUM', symtomps: [1, 5, 6, 15, 25, 29] },
+      { name: 'FARINGITIS', symtomps: [1, 3, 7, 13, 14] },
+      { name: 'KANKER LARING', symtomps: [3, 4, 7, 13, 16, 23, 24] },
+      { name: 'KANKER LEHER DAN KEPALA', symtomps: [3, 12, 15, 21, 30, 31] },
+      { name: 'KANKER LEHER METASTATIK', symtomps: [12] },
+      { name: 'KANKER NASOFARING', symtomps: [5, 15] },
+      { name: 'KANKER TONSIL', symtomps: [7, 12] },
+      { name: 'LARINGITIS', symtomps: [1, 3, 14, 19, 37] },
+      { name: 'NEURONITIS VESTIBULARIS', symtomps: [10, 17] },
+      { name: 'OSTEOSKLEROSIS', symtomps: [20, 35] },
+      { name: 'OTITIS MEDIA AKUT', symtomps: [1, 6, 10, 32] },
+      { name: 'MENIERE', symtomps: [6, 10, 34, 36] },
+      { name: 'TONSILITIS', symtomps: [1, 2, 3, 4, 7, 10] },
+      { name: 'TUMOR SYARAF PENDENGARAN', symtomps: [2, 20, 38] },
+      { name: 'VERTIGO POSTULAR', symtomps: [17] },
+      { name: 'SINUSITIS MAKSILARIS', symtomps: [1, 2, 4, 5, 8, 9, 11, 28, 33] },
+      { name: 'SINUSITIS FRONTALIS', symtomps: [1, 2, 4, 5, 8, 9, 11, 18] },
+      { name: 'SINUSITIS ETMOIDALIS', symtomps: [1, 2, 4, 5, 8, 9, 11, 18, 26, 27] },
+      { name: 'SINUSITIS SFENOIDALIS', symtomps: [1, 2, 4, 5, 6, 8, 9, 11, 12] },
+      { name: 'PERUT', symtomps: [1, 2, 3, 4] },
+    ];
+
+    namaPenyakit.forEach((penyakit) => {
+      if (penyakit.symtomps.includes(selectedSymptomValue)) {
+        if (!presentasePenyakit[penyakit.name]) {
+          presentasePenyakit[penyakit.name] = 0;
         }
+        presentasePenyakit[penyakit.name]++;
+      }
     });
-});
+  });
 
-let listPenyakit = Object.keys(Pencocokan);
-let resultDiv = document.getElementById('result');
-resultDiv.innerHTML = '<h3> Daftar Penyakit </h3>';
-if(listPenyakit.length > 0) {
+  for (let penyakit in presentasePenyakit) {
+    presentasePenyakit[penyakit] = ((presentasePenyakit[penyakit] / totalSelectedSymptoms) * 100).toFixed(2);
+  }
+
+  let totalPresentase = 0;
+  for (let penyakit in presentasePenyakit) {
+    totalPresentase += parseFloat(presentasePenyakit[penyakit]);
+  }
+
+  if (totalPresentase > 100) {
+    for (let penyakit in presentasePenyakit) {
+      presentasePenyakit[penyakit] = ((parseFloat(presentasePenyakit[penyakit]) / totalPresentase) * 100).toFixed(2);
+    }
+    totalPresentase = 100;
+  }
+
+  let resultDiv = document.getElementById('result');
+  resultDiv.innerHTML = '<h3> Presentase Penyakit yang Mungkin Terkena </h3>';
+  if (Object.keys(presentasePenyakit).length > 0) {
     resultDiv.innerHTML += '<ul>';
-        listPenyakit.forEach((penyakit) => {
-            resultDiv.innerHTML += '<li>' + penyakit + '</li>';
-        });
+    for (let penyakit in presentasePenyakit) {
+      resultDiv.innerHTML += '<li>' + penyakit + ': ' + presentasePenyakit[penyakit] + '%</li>';
+    }
     resultDiv.innerHTML += '</ul>';
-
-    resultDiv.innerHTML += '<p> Silahkan berkonsultasi ke dokter jika ingin lebih lanjut... </p>';
-} else {
-    resultDiv.innerHTML += '<p>Tidak ada penyakit</p>'
-}
-
+    // resultDiv.innerHTML += '<p>Total Presentase: ' + totalPresentase.toFixed(2) + '%</p>';
+  } else {
+    resultDiv.innerHTML += '<p>Tidak ada penyakit yang mungkin terkena berdasarkan gejala yang dipilih.</p>';
+  }
 }
